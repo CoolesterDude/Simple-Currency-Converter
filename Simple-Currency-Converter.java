dcb1a31835c9828077b1da1480d568bc32a1bba3
CoolesterDude <Johnsonmblake1@gmail.com>
2025-12-28 23:12:26 -0800

    Moved dynamic variables outside main method to be adjusted in separate methods.

--- Simple-Currency-Converter.java
+++ Simple-Currency-Converter.java
@@ -13,31 +13,31 @@ class Main {
         CURRENCY_RATES.put("euros", 00.85);
         CURRENCY_RATES.put("rupees", 89.55);
     }
+    private static double currency; //If I put these variables inside the main method then they can't be updated in separate methods
+    private static String typeOfCurrency;
 
     public static void main(String[] args) {
-        float currency = 0;
-        double result;
         Scanner input = new Scanner(System.in);
-        String typeOfCurrency = "50";
+        double result;
 
         while(true) {
             System.out.println("Are you converting from USD or to USD? (Input 'From' or 'To')");
             String order = input.nextLine().toLowerCase();
 
             if (order.equals("from")) {
-                result = calculateFromUSD(currency, input);
-                System.out.println("$" + String.format("%.2f", currency) + " is " + String.format("%.2f", result) + " in " + typeOfCurrency);
+                result = calculateFromUSD(input);
+                System.out.println("$" + String.format("%.2f", currency) + " USD is " + String.format("%.2f", result) + " in " + typeOfCurrency);
                 break;
             } else if (order.equals("to")) {
-                result = calculateToUSD(currency, input);
+                result = calculateToUSD(input);
                 System.out.println(String.format("%.2f", currency) + " " + typeOfCurrency + " is " + String.format("%.2f", result) + " USD");
                 break;
             }
         }
     }
-    public static double calculateFromUSD(double currency, Scanner input) {
+    public static double calculateFromUSD(Scanner input) {
         System.out.print("What are you converting to? (Yen, Pesos, Euros, Rupees)\n");
-        String typeOfCurrency = input.nextLine().toLowerCase();
+        typeOfCurrency = input.nextLine().toLowerCase();
 
         double rate = CURRENCY_RATES.get(typeOfCurrency);
 
@@ -47,16 +47,17 @@ class Main {
 
         return currency * rate;
     }
-    public static double calculateToUSD(double currency, Scanner input) {
-        double value = 0;
+    public static double calculateToUSD(Scanner input) {
         System.out.println("What are you converting from? (Yen, Pesos, Euros, Rupees)\n");
         String typeOfCurrency = input.nextLine().toLowerCase();
+
         double rate = CURRENCY_RATES.get(typeOfCurrency);
+
         System.out.println("How much are you converting? (USD)");
-        currency = input.nextFloat();
+        currency = input.nextDouble();
         input.nextLine();
 
-        return value;
+        return currency / rate;
     }
 }
 
