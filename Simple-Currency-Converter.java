9b5f7bfd14edc8a50d4ea7837ef5c365a87ed6ed
CoolesterDude <Johnsonmblake1@gmail.com>
2025-12-28 22:15:35 -0800

    Trying to add methods.

--- Simple-Currency-Converter.java
+++ Simple-Currency-Converter.java
@@ -1,11 +1,21 @@
 package Blake;
 
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 class Main {
+
+    private static final HashMap<String, Double> CURRENCY_RATES = new HashMap<>();
+    static {
+        CURRENCY_RATES.put("yen", 157.42);
+        CURRENCY_RATES.put("pesos", 18.00);
+        CURRENCY_RATES.put("euros", 00.85);
+        CURRENCY_RATES.put("rupees", 89.55);
+    }
+
     public static void main(String[] args) {
-        double yenRate = 157.42;  //Declares Currency rates to Multiply by x
+        double yenRate = 157.42;
         double pesosRate = 18;
         double eurosRate = .85;
         double rupeesRate = 89.55;
@@ -20,69 +30,38 @@ class Main {
             String order = input.nextLine().toLowerCase();
 
             if (order.equals("from")) {
-                result = calculateFromUSD(currency, yenRate, pesosRate, eurosRate, rupeesRate, input);
+                result = calculateFromUSD(currency, input);
                 System.out.println("$" + String.format("%.2f", currency) + " is " + String.format("%.2f", result) + " in " + typeOfCurrency);
                 break;
             } else if (order.equals("to")) {
-                result = calculateToUSD(currency, yenRate, pesosRate, eurosRate, rupeesRate, typeOfCurrency, input);
+                result = calculateToUSD(currency, input);
                 System.out.println(String.format("%.2f", currency) + " " + typeOfCurrency + " is " + String.format("%.2f", result) + " USD");
                 break;
             }
         }
     }
-    public static double calculateFromUSD(float currency, double yenRate, double pesosRate, double eurosRate, double rupeesRate, Scanner input) {
+    public static double calculateFromUSD(float currency, Scanner input) {
         double value = 0;
         System.out.print("What are you converting to? (Yen, Pesos, Euros, Rupees)\n");
         String typeOfCurrency = input.nextLine().toLowerCase();
+        double rate = CURRENCY_RATES.get(typeOfCurrency);
         System.out.println("How much are you converting? (USD)");
         currency = input.nextFloat();
         input.nextLine();  //leaves an extra enter so it's not on the same line
 
-        switch (typeOfCurrency) { //depending on which typeofCurrency the equation of the result changes
-            case ("yen"):
-                value = currency * yenRate;
-                break;
-            case ("pesos"):
-                value = currency * pesosRate;
-                break;
-            case ("euros"):
-                value = currency * eurosRate;
-                break;
-            case ("rupees"):
-                value = currency * rupeesRate;
-                break;
-            default:
-                System.out.println("Please Enter Valid Numbers."); //simple error correction
-                break;
-        }
+        value = currency * rate;
+
         return value;
     }
-    public static double calculateToUSD(float currency, double yenRate, double pesosRate, double eurosRate, double rupeesRate, String typeOfCurrency, Scanner input) {
+    public static double calculateToUSD(float currency, Scanner input) {
         double value = 0;
-        System.out.println("What are you converting from? (Yen, Pesos, Euros, Rupees)");
-        typeOfCurrency = input.nextLine().toLowerCase();
-        input.nextLine();
+        System.out.println("What are you converting from? (Yen, Pesos, Euros, Rupees)\n");
+        String typeOfCurrency = input.nextLine().toLowerCase();
+        double rate = CURRENCY_RATES.get(typeOfCurrency);
         System.out.println("How much are you converting? (USD)");
         currency = input.nextFloat();
         input.nextLine();
 
-        switch (typeOfCurrency) {
-            case ("yen"):
-                value = currency / yenRate;
-                break;
-            case ("pesos"):
-                value = currency / pesosRate;
-                break;
-            case ("euros"):
-                value = currency / eurosRate;
-                break;
-            case ("rupees"):
-                value =currency / rupeesRate;
-                break;
-            default:
-                System.out.println("Please Enter Valid Numbers."); //simple error correction
-                break;
-        }
         return value;
     }
 }
