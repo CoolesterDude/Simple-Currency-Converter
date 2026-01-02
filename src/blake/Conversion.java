ef76d086f50c21337171e069c71d375a001f651f
coolesterdude <johnsonmblake1@gmail.com>
2026-01-02 00:20:13 -0800

    Moved Conversion logic into separate class.

--- src/blake/Conversion.java
+++ src/blake/Conversion.java
@@ -1,40 +1,10 @@
-package Blake;
+package blake;
 
-
-import java.util.HashMap;
 import java.util.Scanner;
 
-class Main {
+import static blake.Main.*;
 
-    private static final HashMap<String, Double> CURRENCY_RATES = new HashMap<>(); //If I put these variables inside the main method then they can't be updated in separate methods
-    static {
-        CURRENCY_RATES.put("yen", 157.42);
-        CURRENCY_RATES.put("pesos", 18.00);
-        CURRENCY_RATES.put("euros", 00.85);
-        CURRENCY_RATES.put("rupees", 89.55);
-    }
-    private static double currency;
-    private static String typeOfCurrency;
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        double result;
-
-        while(true) {
-            System.out.println("Are you converting from USD or to USD? (Input 'From' or 'To')");
-            String order = input.nextLine().toLowerCase();
-
-            if (order.equals("from")) {
-                result = calculateFromUSD(input);
-                System.out.println("$" + String.format("%.2f", currency) + " USD is " + String.format("%.2f", result) + " in " + typeOfCurrency);
-                break;
-            } else if (order.equals("to")) {
-                result = calculateToUSD(input);
-                System.out.println(String.format("%.2f", currency) + " " + typeOfCurrency + " is " + String.format("%.2f", result) + " USD");
-                break;
-            }
-        }
-    }
+public class Conversion {
     public static double calculateFromUSD(Scanner input) {
         double rate;
         while (true) { //Validates Currency
@@ -68,7 +38,7 @@ class Main {
                 rate = CURRENCY_RATES.get(typeOfCurrency); break;
             } else {
                 System.out.println("Please Enter a Valid Form of Currency");
-                }
+            }
         }
         while (true) { //Validates Number
             System.out.println("How much are you converting? (USD)");
@@ -82,4 +52,3 @@ class Main {
         return currency / rate;
     }
 }
-
